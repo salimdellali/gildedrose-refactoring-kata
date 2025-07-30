e956b5f475483b49942b0a8754aba97d15de950e
Salim Dellali <dellali.salim@gmail.com>
2025-07-30 17:22:54 -0400

    focus on using the approval test approach, also generate the current snapshot of the system

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,6 +1,6 @@
 import { Item, GildedRose } from "@/gilded-rose";
 
-describe("Gilded Rose", () => {
+describe.skip("Gilded Rose", () => {
   it("should foo", () => {
     const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
